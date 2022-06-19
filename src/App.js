ed99fd31871930812d43c819421b73d0f9cf3161
onesanity <98060550+onesanity@users.noreply.github.com>
2022-06-19 16:16:35 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather React App</h1>
       <footer>
         This project was coded by <a href="#" target="_blank">Alina Nguyen</a> and is{" "}
 <a href="https://github.com/onesanity/react-weather-app" target="_blank">open sourced on Git Hub</a>.
+    <button>Breathe</button>
     </footer>
+    </div>
     </div>
   );
 }
