642ab28af75d2586658a85e0e8e678782f16fd10
onesanity <98060550+onesanity@users.noreply.github.com>
2022-06-19 13:09:34 +0300

    added Github link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather React App</h1>
-
+      <footer>
+        This project was coded by <a href="#" target="_blank">Alina Nguyen</a> and is{" "}
+<a href="https://github.com/onesanity/react-weather-app" target="_blank">open sourced on Git Hub</a>.
+    </footer>
     </div>
   );
 }
