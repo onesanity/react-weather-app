cdc12765423b0ef23a664db6cd8e5ecc36ba7f2a
onesanity <98060550+onesanity@users.noreply.github.com>
2022-06-24 13:19:04 +0300

    Added new Weather Icon Component  with the right matching icon codes

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Weather from "./Weather";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherSearch(props){
   return (
@@ -10,13 +11,13 @@ export default function WeatherSearch(props){
       <li><FormattedDate date={props.data.currentDate} /></li>
       <li className="text-capitalize">{props.data.description}</li>
     </ul>
-    <div className="row mt-3">
+    <div className="row mt-1">
       <div className="col-6">
         <div className="clearfix">
-        <img 
-        src={props.data.icon} 
-        alt="Sunny" 
-        />
+          <WeatherIcon code={props.data.icon}
+          
+          />
+
   
           <span className="temperature">{Math.round(props.data.temperature)}</span>
           <span className="unit">°C</span>
