77dde75147acac3b4de0a8e18960a466b93f1da1
onesanity <98060550+onesanity@users.noreply.github.com>
2022-06-24 11:00:56 +0300

    replace weather icon URL to a dynamic URL

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props){
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: `https://ssl.gstatic.com/onebox/weather/64/sunny.png`,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     })
 
    ;
